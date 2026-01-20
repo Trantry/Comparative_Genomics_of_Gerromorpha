6d6cc4b633a424e8ddbd6b359eaa3ea0e7cff29b
Trantry <trystan.bessonnier@gmail.com>
2026-01-20 16:16:32 +0100

    maj scratch

--- Scripts/Download_SRA.sh
+++ Scripts/Download_SRA.sh
@@ -4,13 +4,13 @@
 #SBATCH --cpus-per-task=24
 #SBATCH --mem=100G
 #SBATCH --time=05-15:00:00
-#SBATCH --partition=Cascade
+#SBATCH --partition=Lake
 #SBATCH --output=/home/tbessonn/stdout/%A_%a.out # standard output file format
 #SBATCH --error=/home/tbessonn/stderr/%A_%a.err # error file format
 
 
 # Simple : pour chaque */SraAccList.csv, télécharge chaque accession puis convertit en FASTQ dans le même dossier.
-for csv in /home/tbessonn/ressources/RNA_seq/*/SraAccList.csv; do
+for csv in /scratch/Bio/tbessonn/RNA_seq/*/SraAccList.csv; do
   dir=$(dirname "$csv")
   tail -n +2 "$csv" | while read acc; do
     prefetch "$acc" --max-size 1t -O "$dir"
