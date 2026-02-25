d6e82d89d4ec8fba417d6b497efd8b3eecfc586b
Trantry <trystan.bessonnier@gmail.com>
2026-02-25 15:37:05 +0100

    maj

--- Scripts/2_Genome_Annotation/2_Eviann.sh
+++ Scripts/2_Genome_Annotation/2_Eviann.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
-#SBATCH --job-name=M_longipes_Eviann
+#SBATCH --array=1-20
+#SBATCH --job-name=Eviann
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=32
 #SBATCH --mem=120G
 #SBATCH --time=07-00:00:00
-#SBATCH --partition=Lake,Epyc,Lake-premium
+#SBATCH --partition=Cascade,Genoa-premium,Emerald-premium
 #SBATCH --output=/home/tbessonn/stdout/%x_%A_%a.out
 #SBATCH --error=/home/tbessonn/stderr/%x_%A_%a.err
 
@@ -12,57 +13,30 @@ source "${HOME}/miniconda3/etc/profile.d/conda.sh"
 conda activate eviann
 
 #Variables
-WORKDIR=/scratch/Bio/tbessonn/Eviann
-ROOTDIR=/home/tbessonn/ressources/genomes
+WORKDIR=/scratch/Cascade/tbessonn/Eviann
+OUTDIR=/home/tbessonn/1_Eviann
 EVIANN=${HOME}/miniconda3/envs/eviann/bin/eviann.sh
-PROT=/home/tbessonn/ressources/Protein/Heteroptera_Sternorrhyncha_G_buenoi_prot.fasta
-#Transcriptome=/home/tbessonn/ressources/transcriptomes/G_buenoi/gerbue_OGSv1.1_trans.fa
-Transcriptome=/scratch/Bio/tbessonn/transcriptome/M_longipes/10024092.1_Microvelia_longipes.trinity.fa
+PROT=/home/tbessonn/Comparative_Genomics_of_Gerromorpha/Scripts/2_Genome_Annotation/Heteroptera_Sternorrhyncha_G_buenoi_prot.fasta
+TRANSCRIPTOME_FILE=/home/tbessonn/Comparative_Genomics_of_Gerromorpha/Scripts/2_Genome_Annotation/All_transcriptomes_heteroptera.txt
 #For paired-end:
 #paste <(ls $PWD/*_1.fastq) <(ls $PWD/*_2.fastq) > paired.txt
 # For single-end:
 #ls "$PWD"/*.fastq | grep -Ev '(_1|_2)\.fastq$' >> paired.txt
-#RNASEQ=/home/tbessonn/ressources/RNA_seq/Gerromorpha/G_buenoi/paired.txt
-RNASEQ=/home/tbessonn/ressources/RNA_seq/Gerromorpha/M_longipes/paired.txt
+RNASEQ_FILE=/home/tbessonn/Comparative_Genomics_of_Gerromorpha/Scripts/2_Genome_Annotation/All_RNA-seq_heteroptera.txt
+GENOME_FILE=/home/tbessonn/Comparative_Genomics_of_Gerromorpha/Scripts/2_Genome_Annotation/All_genomes_heteroptera.txt
+read KEY GENOME < <(sed -n "${SLURM_ARRAY_TASK_ID}p" "$GENOME_FILE")
+read KEY RNASEQ  < <(sed -n "${SLURM_ARRAY_TASK_ID}p" "$RNASEQ_FILE")
+read KEY TRANSCRIPTOME < <(sed -n "${SLURM_ARRAY_TASK_ID}p" "$TRANSCRIPTOME_FILE")
 
-# building an associative array with the genomes of 9 species of gerromorpha
-declare -A assembly
-#assembly[A_paludum]="$ROOTDIR/gerromorpha/aquarius_paludum/ncbi_dataset/ncbi_dataset/data/GCA_052327185.1/GCA_052327185.1_ASM5232718v1_genomic.fna"
-#assembly[A_najas]="$ROOTDIR/gerromorpha/Aquarius_najas/Anajas_softmasked_genome.fa"
-#assembly[G_buenoi]="$ROOTDIR/gerromorpha/gerris_buenoi/new/genome.softmasked.fa"
-#assembly[G_lacustris]="$ROOTDIR/gerromorpha/gerris_lacustris/ncbi_dataset-2/ncbi_dataset/data/GCA_951217055.1/GCA_951217055.1_ihGerLacu2.1_genomic.fna"
-#assembly[G_odontogaster]="$ROOTDIR/gerromorpha/gerris_odontogaster/gerris_odontogaster_long.PolcaCorrected.sixth_polished.fa"
-#assembly[H_lingyangjiaoensis]="$ROOTDIR/gerromorpha/hermatobates_lingyangjiaoensis/ncbi_dataset/ncbi_dataset/data/GCA_026182355.1/GCA_026182355.1_ASM2618235v1_genomic.fna"
-assembly[M_longipes]="$ROOTDIR/gerromorpha/microvelia_longipes/Mlon_polished_genome_round2.fasta"
-#assembly[R_antilleana]="$ROOTDIR/gerromorpha/rhagovelia_antilleana/unmasked_genome/rhagovelia_antilleana_genome_fourth_polish.fasta.renamed"
-#assembly[T_zetteli]="$ROOTDIR/gerromorpha/tetraripis_zetteli/tzet_genome_flye_v4_unmasked.fasta"
+mkdir -p $WORKDIR/$KEY
+cd $WORKDIR/$KEY
 
-# building an associative array with the genomes of 2 species of non gerromorpha (Nepomorpha)
-#assembly[L_indicus]="$ROOTDIR/non_gerromorpha/nepomorpha/lethocerus_indicus/ncbi_dataset/ncbi_dataset/data/GCA_019843655.1/GCA_019843655.1_UK_Lind_1.0_genomic.fna"
-#assembly[R_chinensis]="$ROOTDIR/non_gerromorpha/nepomorpha/ranatra_chinensis/ncbi_dataset/ncbi_dataset/data/GCA_040954505.1/GCA_040954505.1_ASM4095450v1_genomic.fna"
-
-# building an associative array with the genomes of 5 species of non gerromorpha (Cimicomorpha)
-#assembly[A_suturalis]="$ROOTDIR/non_gerromorpha/cimicomorpha/adelphocoris_suturalis/ncbi_dataset/ncbi_dataset/data/GCA_030762985.1/GCA_030762985.1_ASM3076298v1_genomic.fna"
-#assembly[A_lucorum]="$ROOTDIR/non_gerromorpha/cimicomorpha/apolygus_lucorum/ncbi_dataset/ncbi_dataset/data/GCA_009739505.2/GCA_009739505.2_ASM973950v2_genomic.fna"
-#assembly[C_lectularius_Genbank]="$ROOTDIR/non_gerromorpha/cimicomorpha/cimex_lectularius/ncbi_dataset/ncbi_dataset/data/GCA_000648675.3/GCA_000648675.3_Clec_2.1_genomic.fna"
-#assembly[R_fuscipes]="$ROOTDIR/non_gerromorpha/cimicomorpha/rhynocoris_fuscipes/ncbi_dataset/ncbi_dataset/data/GCA_040020575.1/GCA_040020575.1_Rfu_1.0_genomic.fna"
-#assembly[T_infestans]="$ROOTDIR/non_gerromorpha/cimicomorpha/triatoma_infestans/ncbi_dataset/ncbi_dataset/data/GCA_965641795.1/GCA_965641795.1_ihTriInfe1.hap1.1_genomic.fna"
-
-# building an associative array with the genomes of 5 species of non gerromorpha (Pentatomomorpha)
-#assembly[P_apterus]="$ROOTDIR/non_gerromorpha/pentatomomorpha/pyrrhocoris_apterus/ncbi_dataset/ncbi_dataset/data/GCA_039877355.1/GCA_039877355.1_ASM3987735v1_genomic.fna"
-#assembly[E_furcellata]="$ROOTDIR/non_gerromorpha/pentatomomorpha/eocanthecona_furcellata/ncbi_dataset/ncbi_dataset/data/GCA_052550355.1/GCA_052550355.1_ASM5255035v1_genomic.fna"
-#assembly[G_acuteangulatus]="$ROOTDIR/non_gerromorpha/pentatomomorpha/gonocerus_acuteangulatus/ncbi_dataset/ncbi_dataset/data/GCA_946811695.1/GCA_946811695.1_ihGonAcut1.1_genomic.fna"
-#assembly[O_fasciatus]="$ROOTDIR/non_gerromorpha/pentatomomorpha/oncopeltus_fasciatus/ncbi_dataset/ncbi_dataset/data/GCA_000696205.2/GCA_000696205.2_Ofas_2.0_genomic.fna"
-#assembly[R_pedestris]="$ROOTDIR/non_gerromorpha/pentatomomorpha/riptortus_pedestris/ncbi_dataset/ncbi_dataset/data/GCA_019009955.1/GCA_019009955.1_ASM1900995v1_genomic.fna"
-
-for key in "${!assembly[@]}"
-do
-    GENOME=${assembly[$key]}
-    mkdir -p $WORKDIR/$key
-    cd $WORKDIR/$key
+if ls "$OUTDIR/$KEY"/*.gff 1> /dev/null 2>&1; then
+    echo "Skipping $KEY: .gff found in $OUTDIR/$KEY"
+else
     $EVIANN -g $GENOME \
             -p $PROT \
             -r $RNASEQ \
-            -e $Transcriptome \
+            -e $TRANSCRIPTOME \
             -t 32
-done
+fi
